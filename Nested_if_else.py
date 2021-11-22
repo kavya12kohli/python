c0101828c03d1d00fa5c234f03edb17ca8124fd1
KAVYA KOHLI <94300409+kavya12kohli@users.noreply.github.com>
2021-11-22 14:27:37 +0530

    Add files via upload

---
+++ Nested_if_else.py
@@ -0,0 +1,11 @@
+x=12
+if x>15:
+   print("It is not my birthday.")
+elif x<15:
+   print("Its my birthday!")
+else:
+    print("Its a regular monday people.")
+if x<6:
+    print("I'm not having a party.")
+else:
+    print("I'm expecting to see you bearing gifts. Lots of them.")
