c525646b6b06f16a51855a043e73875ac222a6f0
KAVYA KOHLI <94300409+kavya12kohli@users.noreply.github.com>
2021-11-24 19:53:31 +0530

    Add files via upload

---
+++ positive_integers_from_list.py
@@ -0,0 +1,15 @@
+list1=[12,-7,5,64,-14]
+print("list1=[12,-7,5,64,-14]")
+print("The positive integers are:")
+for number1 in list1:
+    if number1>0:
+         print(number1)
+
+print("")
+
+list2=[12,14,-95,3]
+print("list2=[12,14,-95,3]")
+print("The positive integers are:")
+for number2 in list2:
+    if number2>0:
+        print(number2)
